5e450256e9de603f2dcd47290734fcd8c009a97d
Ansters26 <125726133+Ansters26@users.noreply.github.com>
2024-01-23 19:43:42 +0000

    day-2

---
+++ basics/01_var.js
@@ -0,0 +1,16 @@
+const accountId = 12332//const is a keyword which assigns memory some data like accountId is memory name and 12332 is memory data
+let emailId = "atha@google.com"
+var accountPass="8655"
+accountCity = "Nagpur"
+let acccountState;//if no value is assigned we get undefined
+
+
+//const accountId=2  not allowed because const have non cahngeable values
+
+/*
+prefer not to use var 
+because of scope resolution in blocks and functions
+*/
+
+console.log(accountId)
+console.table([accountId,emailId,accountPass,accountCity,acccountState])
