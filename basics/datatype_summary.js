779194ba65360f6cc05de2d5d3aa832b44a35bc9
Ansters26 <125726133+Ansters26@users.noreply.github.com>
2024-01-23 19:54:50 +0000

    done with the summary

---
+++ basics/datatype_summary.js
@@ -0,0 +1,17 @@
+//Primitive 
+
+// 7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);//triple equal to compare type and value
+
+
+
+//Reference(Non Primitive)
+
+//Array, Objects, Functions
